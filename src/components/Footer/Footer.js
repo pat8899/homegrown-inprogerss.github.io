f2f3a7b0faac940435940ed49e74a266547c1acf
Pat <peeh.indy@gmail.com>
2022-02-06 20:22:56 +1300

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Footer() {
+    return (
+        <footer class="page-footer font-small blue footer">
+
+            <div class="footer-copyright text-center py-3">Made with ❤ by
+        <a href="https://github.com/pat8899" target="_blank" rel="noopener noreferrer" className="author"> PPAT</a>
+            </div>
+
+        </footer>
+    );
+}
+
+export default Footer;
